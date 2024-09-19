b07fe80a17847b18bc847b26e4c300244876f7f0
Imran Fazli <95630389+Beanlek@users.noreply.github.com>
2024-09-19 15:08:41 +0800

    Merge [feature-amar]

---
+++ lib/streams/general_stream.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class GeneralStreams{
+
+static StreamController<Locale> languageStream = StreamController<Locale>.broadcast(); 
+
+}
